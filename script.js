72e8748dbb43e8f9d209bbf4dd0f456a01004569
Harry Faulkner <hfaulkner2006@gmail.com>
2026-02-18 00:42:32 +0000

    Modernise 'locate' button

--- script.js
+++ script.js
@@ -16,15 +16,32 @@
 //   console.error("geolocation not supported");
 // }
 
+document.querySelector("#locate").addEventListener("click", setStation);
+
 function setStation() {
   const stat_name = document.querySelector("#stat_name");
   const stat_code = document.querySelector("#stat_code");
 
+  const from = document.querySelector("#from");
+  const to = document.querySelector("#to");
+
+  const op_name = document.querySelector("#op_name");
+
   if (stat_name.textContent == "Portsmouth Harbour") {
     stat_name.textContent = "Rhoose Cardiff International Airport";
     stat_code.textContent = "RIA";
+
+    from.textContent = "RIA";
+    to.textContent = "CDF";
+
+    op_name.textContent = "Transport for Wales";
   } else {
     stat_name.textContent = "Portsmouth Harbour";
     stat_code.textContent = "PMH";
+
+    from.textContent = "PMH";
+    to.textContent = "WAT";
+
+    op_name.textContent = "South Western Railway";
   }
 }
